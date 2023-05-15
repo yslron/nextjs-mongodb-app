d61f16f43eb9c6ac58fb1db30a880c006a436087
Elron <yslron1000@gmail.com>
2023-05-16 05:17:24 +0800

    deleted delete db

--- api-lib/db/delete.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { ObjectId } from 'mongodb';
-
-export async function deletePostById(db, id) {
-  const result = await db.collection('posts').deleteOne({ _id: new ObjectId(id) });
-  return result.deletedCount > 0;
-}
